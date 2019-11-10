ad3d36aa9749af5fbfac68e23c90427284998a9d
munene19 <munenexv@gmail.com>
2019-11-11 00:56:15 +0300

    Add new page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-function formation(){
-    window.open("../landing.html")
+function totalamount(){
+
 }
-// var swiper = new Swiper('.swiper-container', {
-//     effect: 'coverflow',
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: 'auto',
-//     coverflowEffect: {
-//       rotate: 50,
-//       stretch: 0,
-//       depth: 100,
-//       modifier: 1,
-//       slideShadows : true,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//   });
