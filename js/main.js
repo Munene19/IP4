ed5a6de5306a53437fb62aedb5dfa278b504d259
munene19 <munenexv@gmail.com>
2019-11-11 12:24:10 +0300

    Add new pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function totalamount(){
-
-}
+// function getCrustValue(){
+//     var getCrustValue = document.getElementById('crust')
+// }
+$(document).ready(
+    $("#large").click(
+        function(){
+           var price = $(this).val();
+            alert(price);
+        }
+    )
+)
